c7bdd4316846eecf71478832701d0204b666f452
Subhajit Ganguly <subhajitganguly53@gmail.com>
2024-07-24 20:34:18 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name='bonecrack_detection',
+      version='0.0.0',
+      author='Subhajit Ganguly',
+      author_email='subhajitganguly53@gmail.com',
+      packages=find_packages(),
+      install_requires = [])
